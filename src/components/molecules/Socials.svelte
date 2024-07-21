838d834b285b4fde786b1e77a1bf928576b7eafb
matilde-silva-21 <matilde.m.silva.21@gmail.com>
2024-07-21 16:01:24 +0100

    update socials

--- src/components/molecules/Socials.svelte
+++ src/components/molecules/Socials.svelte
@@ -1,28 +1,22 @@
 <script>
 	import Social from '../atoms/Social.svelte';
-	import { user } from '../../util/discord';
-
 	import { GitHubIcon } from '@indaco/svelte-iconoir/github';
-	import { DiscordIcon } from '@indaco/svelte-iconoir/discord';
 	import { MailIcon } from '@indaco/svelte-iconoir/mail';
-	import { CoffeeCupIcon } from '@indaco/svelte-iconoir/coffee-cup';
+	import Icon from '@iconify/svelte';
+	
 </script>
 
 <div class="socials-container">
-	<Social tip="xafn" link="https://github.com/xafn">
+	<Social tip="matilde-silva-21" link="https://github.com/matilde-silva-21">
 		<GitHubIcon color="var(--accent)" />
 	</Social>
 
-	<Social tip={user.username} link={`https://discord.com/users/${user.id}`}>
-		<DiscordIcon color="var(--accent)" />
-	</Social>
-
-	<Social tip="hey@afn.im" link="mailto:hey@afn.im">
+	<Social tip="matilde.m.silva.21@gmail.com" link="mailto:matilde.m.silva.21@gmail.com">
 		<MailIcon color="var(--accent)" />
 	</Social>
 
-	<Social tip="affan" link="https://ko-fi.com/affan">
-		<CoffeeCupIcon color="var(--accent)" />
+	<Social tip="matilde-m-silva" link="https://www.linkedin.com/in/matilde-m-silva/">
+		<Icon icon="uil:linkedin" color="var(--accent)" font-size="28px"/>
 	</Social>
 </div>
 
